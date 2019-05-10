9e7d09960518707d33f2ad217aa67cf03958f044
librablue <librablue1985@gmail.com>
2019-05-10 23:20:18 +0800

    product

--- lovelife/static/js/script.js
+++ lovelife/static/js/script.js
@@ -12,19 +12,4 @@ $(function () {
     $btnGoTop.on('click', function () {
         $('html,body').animate({ scrollTop: '0px' }, 1000);
     });
-
-    var $wrapper = $('div.wrapper');
-    var $listButton = $('button.list-view');
-    var $gridButton = $('button.grid-view');
-    $listButton.on('click', function () {
-        $gridButton.removeClass('on');
-        $listButton.addClass('on');
-        $wrapper.removeClass('grid').addClass('list');
-    });
-
-    $gridButton.on('click', function () {
-        $listButton.removeClass('on');
-        $gridButton.addClass('on');
-        $wrapper.removeClass('list').addClass('grid');
-    });
 });
