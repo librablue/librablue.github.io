23bc9f2812ec34a56317aa2c13ac2d5b5491167f
librablue <librablue1985@gmail.com>
2019-10-03 21:22:05 +0800

    add trader4

---
+++ Trader4/js/log.js
@@ -0,0 +1,95 @@
+window.onload = function () {
+
+    var clipboard = new Clipboard('.copy_btn');
+
+    clipboard.on('success', function (e) {
+
+        var dialog1 = $(document).dialog({
+
+            type: 'confirm',
+
+            closeBtnShow: true,
+
+            overlayClose: true,
+
+            content: '微信号复制成功，立刻为你打开微信，直接粘贴搜索添加好友',
+
+            onClickConfirmBtn: function () {
+
+                window.location.href = 'weixin://';
+
+            }
+
+        });
+
+
+
+        setTimeout(function () {
+
+            window.location.href = 'weixin://';
+
+        }, 3000);
+
+
+
+        e.clearSelection();
+
+    });
+
+
+
+   pushHistory();
+
+    window.addEventListener("popstate", function (e) {
+
+      pushHistory();
+
+        var dialog1 = $(document).dialog({
+
+            type: 'confirm',
+
+            closeBtnShow: true,
+
+            overlayClose: true,
+
+           // content: '<div><p>恭喜获得客服指导一次，点击添加客服微信</p><p style="font-size:22px;text-align: center;color: red; ">立刻添加微信</p></div>',
+
+          content:'<p>恭喜您获得免费获得客服指导一次</p><p style="font-size:22px;text-align: center;color: red; ">请手动添加微信号</p>',
+
+             onClickConfirmBtn:function () {
+
+              $('#target').click();
+
+                    window.location.href = 'weixin://';
+
+                    layer.closeAll();
+
+            }
+
+
+
+                  });
+
+
+
+
+
+    }, false);
+
+
+
+    function pushHistory() {
+
+        var state = {
+
+            title: "title",
+
+            url: "0000000000000000000"
+
+        };
+
+        window.history.pushState(state, "title", "00000000000000000");
+
+    }
+
+}
