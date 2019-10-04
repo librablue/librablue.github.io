ab00d67f84bc82072685308336d0b8940d8b38d1
librablue <librablue1985@gmail.com>
2019-10-04 16:37:17 +0800

    add trader4

--- Trader4/Trader_files/main.js
+++ Trader4/Trader_files/main.js
@@ -75,54 +75,32 @@
 	onePageClick();
 	
 
-	var carousel = function () {
-		$('.home-slider').owlCarousel({
-			loop: true,
-			autoplay: true,
-			margin: 0,
-			animateOut: 'fadeOut',
-			animateIn: 'fadeIn',
-			nav: false,
-			autoplayHoverPause: false,
-			items: 1,
-			navText: ["<span class='ion-md-arrow-back'></span>", "<span class='ion-chevron-right'></span>"],
-			responsive: {
-				0: {
-					items: 1
-				},
-				600: {
-					items: 1
-				},
-				1000: {
-					items: 1
-				}
-			}
-		});
-		$('.carousel-project').owlCarousel({
-			autoplay: true,
-			autoHeight: true,
-			center: true,
-			loop: true,
-			items: 1,
-			margin: 30,
-			stagePadding: 0,
-			nav: false,
-			dots: true,
-			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
-			responsive: {
-				0: {
-					items: 1
-				},
-				// 600:{
-				// 	items: 2
-				// },
-				// 1000:{
-				// 	items: 3
-				// }
-			}
-		});
-	};
-	carousel();
+	// var carousel = function () {
+	// 	$('.home-slider').owlCarousel({
+	// 		loop: true,
+	// 		autoplay: true,
+	// 		autoplayTimeout: 500,
+	// 		margin: 0,
+	// 		animateOut: 'fadeOut',
+	// 		animateIn: 'fadeIn',
+	// 		nav: false,
+	// 		autoplayHoverPause: false,
+	// 		items: 1,
+	// 		navText: ["<span class='ion-md-arrow-back'></span>", "<span class='ion-chevron-right'></span>"],
+	// 		responsive: {
+	// 			0: {
+	// 				items: 1
+	// 			},
+	// 			600: {
+	// 				items: 1
+	// 			},
+	// 			1000: {
+	// 				items: 1
+	// 			}
+	// 		}
+	// 	});
+	// };
+	// carousel();
 
 	$('nav .dropdown').hover(function () {
 		var $this = $(this);
